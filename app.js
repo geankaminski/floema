ccd3f6dbf1dcac99279ba412c31f1960deaab921
Gean Kaminski <geankaminski93@hotmail.com>
2023-01-29 21:39:23 -0300

    adding pug files

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/", (req, res) => {
+  res.render("pages/home", {
+    meta: {
+      data: {
+        title: "Floema",
+        description: "Metadata description",
+      },
+    },
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about", {
+    meta: {
+      data: {
+        title: "Floema",
+        description: "Metadata description",
+      },
+    },
+  });
+});
+
+app.get("/collections", (req, res) => {
+  res.render("pages/collections", {
+    meta: {
+      data: {
+        title: "Floema",
+        description: "Metadata description",
+      },
+    },
+  });
+});
+
+app.get("/detail/:id", (req, res) => {
+  res.render("pages/detail", {
+    meta: {
+      data: {
+        title: "Floema",
+        description: "Metadata description",
+      },
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
